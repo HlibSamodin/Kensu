e687ff403f20a66d0d8b56213bf4a0f015dc4a86
HlibSamodin <hlibsamodin11@gmail.com>
2026-06-10 17:28:59 +0100

    mini fragility fix for future

--- collection/label.py
+++ collection/label.py
@@ -13,7 +13,7 @@ def is_correct(response, answer):
     # check if the answer we get is anywhere in the actual response of ai
     norm_answer = normalise(answer)
     norm_response = normalise(response)
-    if norm_answer in norm_response:
+    if re.search(r'\b' + re.escape(norm_answer) + r'\b', norm_response):
         return True
     else:
         return False
